cef1ef32c4424bd1e9c100623d2064f9a9695ee6
Rayan Derenkai <rderenka0@uci.edu>
2026-06-21 01:18:55 -0700

    More than Half way

--- main.py
+++ main.py
@@ -36,8 +36,22 @@ class CSV:
         df["date"] = pd.to_datetime(df["date"], format: CSV.FORMAT)
         start_date = datetime.strptime(start_date, CSV.FORMAT)
         end_date = datetime.strptime(end_date, CSV.FORMAT)
-        
 
+        mask = (df["date"] >= start_date) & (d["date"] <= end_date) 
+        filtered_df = df.loc[mask]
+
+        if filtered_df.empty:
+            print("No transactions found in the given date range")
+        else:
+            print(f"Transactions from {start_date.strftime(CSV.FORMAT)} to {end_date.strftime(CSV.FORMAT)}")
+            print(filtered_df.to_string(index=False, formatters={"date": lambda x: x.strftime(CSV.FORMAT)}))
+
+            total_income = filtered_df[filtered_df["category"] == "Income"]["amount"].sum()
+            total_expense = filtered_df[filtered_df["category"] == "Expense"]["amount"].sum()
+            print("\nSummary:")
+            print(f"Total Income: ${total_income:.2f}")
+            print(f"Total Expense: ${total_expense:.2f}")
+            print(f"Net Savings: ${(total_income - total_expense):.2f}")
 
 def add():
     CSV.initialize_csv()
